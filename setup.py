4f86d82602b6c82e7d67125417caf4bdecae5357
Aleksander Sadowski <47467484+alekssadowski95@users.noreply.github.com>
2021-03-12 21:22:20 +0100

    pypi build

--- setup.py
+++ setup.py
@@ -2,14 +2,6 @@
 
 from setuptools import setup, find_packages
 
-classifiers = [
-  'Development Status :: 4 - Beta',
-  'Intended Audience :: Education',
-  'Operating System :: Microsoft :: Windows :: Windows 10',
-  'License :: OSI Approved :: MIT License',
-  'Programming Language :: Python :: 3'
-]
-
 
 with open('README.md') as f:
     readme = f.read()
@@ -19,13 +11,20 @@ with open('LICENSE') as f:
 
 setup(
     name='simple-blockchain',
-    version='0.1.0',
+    version='0.1.1',
     description='This is a simplified model of a blockchain, which is also the technology behind the well known crypto-currency Bitcoin.',
     long_description=readme,
     author='Aleksander Sadowski',
     author_email='aleksander.sadowski@alsado.de',
     url='https://github.com/alekssadowski95/simple-blockchain',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Education',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
 )
 
