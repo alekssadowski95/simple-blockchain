e155e0714cad622e1473e376f89852c02f0bfa54
Aleksander Sadowski <47467484+alekssadowski95@users.noreply.github.com>
2021-03-12 22:28:15 +0100

    refactoring

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Aleksander Sadowski'
 author = 'Aleksander Sadowski'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
