6435adf9d94cf15ae8b5b1d41fb99a7a9a5b9f9b
Aleksander Sadowski <47467484+alekssadowski95@users.noreply.github.com>
2021-03-12 20:51:33 +0100

    setup classifiers

--- setup.py
+++ setup.py
@@ -2,6 +2,14 @@
 
 from setuptools import setup, find_packages
 
+classifiers = [
+  'Development Status :: 4 - Beta',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
 
 with open('README.md') as f:
     readme = f.read()
